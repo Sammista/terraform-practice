fef0878f83b24c1ad83f67038591c176d72a6c70
Sammie stairs <132995722+Sammista@users.noreply.github.com>
2023-05-26 15:41:59 +0100

    ran terraform fmt

--- Challenge_1/terraform config/main.tf
+++ Challenge_1/terraform config/main.tf
@@ -1,7 +1,7 @@
 #Providers
 
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
   access_key = var.access_key
   secret_key = var.secret_key
 }
@@ -9,12 +9,12 @@ provider "aws" {
 #Data 
 #Get AMI instance from AWS, make sure it's the most recent version. This will be referenced when creating the EC2.
 
-data "aws_ami" "amazon-linux-2"{
-most_recent = true
- filter {
-   name   = "name"
-   values = ["amzn2-ami-hvm*"]
- }
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
 
 }
 
@@ -23,50 +23,50 @@ most_recent = true
 #create private key
 #Then create keypair
 resource "tls_private_key" "privatekey" {
-algorithm = "RSA"
+  algorithm = "RSA"
 }
 resource "aws_key_pair" "keypair" {
-key_name = "keypair"
-#take the public key from the generated private key.
-public_key = tls_private_key.privatekey.public_key_openssh
-#Depends on private key being created
-depends_on = [ tls_private_key.privatekey ]
+  key_name = "keypair"
+  #take the public key from the generated private key.
+  public_key = tls_private_key.privatekey.public_key_openssh
+  #Depends on private key being created
+  depends_on = [tls_private_key.privatekey]
 }
 # Save the local version to your working directoy.
 resource "local_file" "ssh_key" {
-content =tls_private_key.privatekey.private_key_pem
-filename = "keypair2"
-  
+  content  = tls_private_key.privatekey.private_key_pem
+  filename = "keypair2"
+
 }
 
 
 resource "aws_vpc" "vpc1" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
 }
 
 
 #In order to access the internet there also needs be an internet gateway, this can be retrieved from the aws_vpc object created above.
 resource "aws_internet_gateway" "internetgateway" {
-    vpc_id = aws_vpc.vpc1.id
-  
+  vpc_id = aws_vpc.vpc1.id
+
 }
 
 #Create a subnet and use map_public_ip_on_launch so the EC2 has a public IP address as per the brief. Reference CIDR block from VPC.
 
 resource "aws_subnet" "subnet" {
-cidr_block = aws_vpc.vpc1.cidr_block
-vpc_id = aws_vpc.vpc1.id
-map_public_ip_on_launch = "true"
+  cidr_block              = aws_vpc.vpc1.cidr_block
+  vpc_id                  = aws_vpc.vpc1.id
+  map_public_ip_on_launch = "true"
 }
 
 resource "aws_route_table" "routetable" {
-vpc_id = aws_vpc.vpc1.id
+  vpc_id = aws_vpc.vpc1.id
 
-#Define the route inside of the route table with the created internet gateway. We don't really need to define this seperate as we only have one route.
-route {
- cidr_block = "0.0.0.0/0"
- gateway_id = aws_internet_gateway.internetgateway.id
+  #Define the route inside of the route table with the created internet gateway. We don't really need to define this seperate as we only have one route.
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internetgateway.id
   }
 
 }
@@ -81,35 +81,35 @@ resource "aws_route_table_association" "rta" {
 
 #Create a security group and reference the VPC. Allow inbound SSH connections only.
 
-resource "aws_security_group" "aws_security_group"{
-name = "allowssh"
+resource "aws_security_group" "aws_security_group" {
+  name = "allowssh"
 
-vpc_id = aws_vpc.vpc1.id
-ingress {
+  vpc_id = aws_vpc.vpc1.id
+  ingress {
 
-protocol = "TCP"
-from_port = "22"
-to_port = "22"
-cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "TCP"
+    from_port   = "22"
+    to_port     = "22"
+    cidr_blocks = ["0.0.0.0/0"]
 
-}
+  }
 
 }
 
 resource "aws_instance" "ec2instance" {
-ami = "${data.aws_ami.amazon-linux-2.id}"
-instance_type = "t2.micro"
-subnet_id =  "${aws_subnet.subnet.id}"
-key_name = "keypair"
- vpc_security_group_ids = [aws_security_group.aws_security_group.id]
+  ami                    = data.aws_ami.amazon-linux-2.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.subnet.id
+  key_name               = "keypair"
+  vpc_security_group_ids = [aws_security_group.aws_security_group.id]
 }
 
 #Output public DNS to SSH onto the EC2 instance.
 output "aws_instance_public_dns" {
-value = aws_instance.ec2instance.public_dns 
+  value = aws_instance.ec2instance.public_dns
 }
 
 output "locationofpemfile" {
-value = path.cwd
+  value = path.cwd
 }
 
--- Challenge_1/terraform config/variables.tf
+++ Challenge_1/terraform config/variables.tf
@@ -2,13 +2,13 @@
 # We don't want to store the values in here because that isn't secure.
 # use the var flag when running terraform commands and supply the values. See the readme for more details.
 variable "access_key" {
-type =string
-sensitive = true
+  type      = string
+  sensitive = true
 
 }
 
 variable "secret_key" {
-type =string
-sensitive = true
+  type      = string
+  sensitive = true
 
 }
