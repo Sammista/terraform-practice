1551b2e043ebd4535dece93e2275ff8ef0f32960
Sammie stairs <132995722+Sammista@users.noreply.github.com>
2023-06-02 15:05:38 +0100

    added gitignore file and deleted variables for aws

--- Challenge_1/terraform config/main.tf
+++ Challenge_1/terraform config/main.tf
@@ -2,8 +2,8 @@
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
+  #access_key = var.access_key
+  #secret_key = var.secret_key
 }
 
 #Data 
